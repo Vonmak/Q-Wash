730a1e2ffb24c18fa5a2c4b80b635767aece0057
Guest <von.makori@gmail.com>
2024-01-12 18:51:37 +0300

    update main with vuesnip

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
+import VueSnip from 'vue-snip'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(VueSnip).mount('#app')
